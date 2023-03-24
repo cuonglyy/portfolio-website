ca6f37c7bfe030ead026426adeb8963d245d9f0f
Cuong Ly <cuonglyy@gmail.com>
2023-03-24 15:14:16 -0700

    Created main and footer component to add semantic meaning to html document

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+    Box,
+} from '@chakra-ui/react';
+
+
+function Footer() {
+    return (
+        <Box as="footer">
+
+        </Box>
+    );
+}
+
+
+export default Footer;
---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+    Box,
+} from '@chakra-ui/react';
+
+/* 
+    This component is meant to give semantic HTML for each page
+*/
+function Main() {
+    return (
+        <Box as="main" mx={[8, 10 ,41.25]}>
+
+        </Box>
+    );
+}
+
+export default Main;
